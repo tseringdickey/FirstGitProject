41be2241ca8623442c5fe93a0c3c957f353baf16
tseringdickey <c_ring@live.com>
2025-10-23 15:15:48 -0400

    Update HelloWorld.java
    Added extra souts.

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -5,5 +5,10 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("Hello orld");
+        System.out.println("Hello Word");
+        System.out.println("Hello Wrld");
+
+
     }
 }
